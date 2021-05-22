61e7063c52a8eb5d21c79abec24f037b6f259c88
Kai Salmon <kaisalmon@hotmail.com>
2021-05-22 13:29:34 +0100

    Running very well

--- run.py
+++ run.py
@@ -1,29 +1,64 @@
 import cv2
 import glob
 import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib.patches import Rectangle
+from mtcnn import MTCNN
 from skimage.transform import match_histograms
+import imageio
+import datetime
+from datetime import timedelta
+
+
+model = MTCNN()
 
 imageDir = "./images/*.*"
 cascPath = "./cascade.xml"
 ref_image_path = "./images/day4.jpg"
-reference_image = cv2.imread(ref_image_path)
-target_face_size = 360
-
+target_face_size = 600
+fps = 6
 
 faceCascade = cv2.CascadeClassifier(cascPath)
 
 
-def process_image(imagePath):
+def process_image(imagePath, number):
     image = cv2.imread(imagePath)
-    image = scale_image(image, 50)
-    image = whitepatch_balancing(image, 200, 100, 40, 40, 0.4)
-    image = color_match(image, reference_image, 0.3)
-    image = detect_face(image)
+    image = first_step(image)
+    image = color_match(image, reference_image, 0)
     image = crop_image(image, 80)
+    image = add_text(image, number)
     return image
 
+
+def first_step(image):
+    image = detect_face(image)
+    image = scale_image(image, 50)
+    image = whitepatch_balancing(image, 200, 100, 40, 40, 0.7)
+    return image
+
+
+def add_text(image, number):
+    th = 60
+    cv2.rectangle(image, (0,0), (image.shape[1], th), (0, 0, 0), -1)
+    # fontScale
+    fontScale = 1
+
+    org = (8, th-16)
+
+    # Red color in BGR
+    color = (1.,1.,1.)
+
+    # Line thickness of 2 px
+    thickness = 2
+
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    cv2.putText(image, "Day "+f'{number:03}', org, font, fontScale,
+                        color, thickness, cv2.LINE_AA, False)
+
+    date = datetime.datetime.strptime('2021-05-16', '%Y-%m-%d') + timedelta(days=number)
+    org = (370, th-16)
+    cv2.putText(image, str(date.date()), org, font, fontScale,
+                        color, thickness, cv2.LINE_AA, False)
+
+    return image
 
 def whitepatch_balancing(image, from_row, from_column, row_width, column_width, strength):
 
@@ -49,7 +84,7 @@ def crop_image(image, pixels):
     return image[y:y + h, x:x + w]
 
 def color_match(image, reference, strength):
-    color_matched = match_histograms(image, reference/255, multichannel=True)
+    color_matched = match_histograms(image, reference, multichannel=True)
     beta = 1.0 - strength
     image = cv2.addWeighted(color_matched, strength, image, beta, 0.0)
     return image
@@ -63,24 +98,18 @@ def scale_image(image, scale_percent):
 
 
 def detect_face(image):
-    gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-    gray = (gray * 255 ).astype(np.uint8)
-    faces = faceCascade.detectMultiScale(
-        gray,
-        minNeighbors=1,
-        minSize=(300, 300),
-        flags=cv2.CASCADE_SCALE_IMAGE
-    )
+    faces = model.detect_faces(image)
     print("Found {0} faces!".format(len(faces)))
 
     # Draw a rectangle around the faces
     if len(faces) == 0:
-        raise Exception("Couldn't detect face")
-    face = faces[0]
+        return image
+        #raise Exception("Couldn't detect face")
+    face = faces[0]['box']
     (x, y, w, h) = face
     #print(x,y,w,h)
 
-    cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    #cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
     avg_face_dim = (h+w)/2;
     scale = target_face_size/avg_face_dim
@@ -106,11 +135,29 @@ def detect_face(image):
 
     return scaled_and_centered
 
+reference_image = first_step(cv2.imread(ref_image_path))
 images = glob.glob(imageDir)
-print(images)
-for path in images:
-    image = process_image(path)
-    cv2.imshow("Faces found", image)
-    cv2.waitKey(100)
 
-cv2.waitKey(500)
+if True:
+    with imageio.get_writer("output/input.gif", mode="I", fps=fps) as writer:
+        idx = 0
+        for path in images:
+            frame_length = 1
+            if idx == 0 or idx == len(images) - 1:
+                frame_length = 4
+            image = scale_image(cv2.imread(path), 50)
+            for i in range(0, frame_length):
+                writer.append_data(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+            idx += 1
+
+with imageio.get_writer("output/output.gif", mode="I",  fps=fps) as writer:
+    idx = 0
+    for path in images:
+        frame_length = 1
+        if idx == 0 or idx == len(images) - 1:
+            frame_length = 4
+        image = process_image(path, idx+1) * 255
+        cv2.imwrite(path.replace("images", "output"), image)
+        for i in range(0, frame_length):
+            writer.append_data(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+        idx += 1
